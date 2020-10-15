d16858a255f19e8e2238976163d05e9668167d94
Bineeta Oram <54524155+Bineeta2158@users.noreply.github.com>
2020-10-15 13:35:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,16 @@ $(document).ready(function(){
     }]
   });
 });
+shareButton.addEventListener('click', event => {
+  if (navigator.share) {
+    navigator.share({
+      title: 'WebShare API Demo',
+      url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+    .catch(console.error);
+  } else {
+    // fallback
+  }
+});
